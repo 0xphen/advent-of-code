886401c4be0dd255d451aeab1741bb3af51a0e74
0xphen <raph.kifen@gmail.com>
2023-07-29 20:45:37 +0200

    Solve part 2 of day 4

--- src/solutions/day_4/solution.rs
+++ src/solutions/day_4/solution.rs
@@ -28,6 +28,23 @@ impl<'a> Day4 {
         return count;
     }
 
+    // TODO: remove duplicate code in part_1 and part_2
+    fn part_2(&self) -> u32 {
+        let mut count = 0;
+
+        for (k, v) in &self.pair_map {
+            let (largest_vec, smallest_vec) = Day4::largest_vec(&v[0], &v[1]);
+            let set_0: HashSet<_> = largest_vec.iter().collect();
+            let is_subset = smallest_vec.iter().any(|item| set_0.contains(item));
+
+            if is_subset {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
     pub fn solution() -> Solution {
         let file = File::open("src/inputs/day_4_input.txt").expect("Failed to open file");
         let lines = BufReader::new(file).lines();
@@ -43,10 +60,11 @@ impl<'a> Day4 {
 
         let mut day_4 = Day4::new(pair_map);
         let part_1_soln = day_4.part_1();
+        let part_2_soln = day_4.part_2();
 
         return Solution {
             part_1_soln: part_1_soln as u64,
-            part_2_soln: 0,
+            part_2_soln: part_2_soln as u64,
         };
     }
 
