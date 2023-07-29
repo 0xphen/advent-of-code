22741c8558b2077743152964447670ddffec4243
0xphen <raph.kifen@gmail.com>
2023-07-29 20:34:29 +0200

    Solve part 1 of day 4

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,7 @@ fn main() {
     let day_3_solution = day_3::solution::Day3::solution();
     println!("Day 3 solution : {:?} ", day_3_solution);
 
-    //day_3::solution::Day3::solution();
+    // Day 4
+    let day_4_solution = day_4::solution::Day4::solution();
+    println!("Day 4 solution : {:?} ", day_4_solution);
 }
---
+++ src/solutions/day_4/solution.rs
@@ -0,0 +1,76 @@
+use crate::solutions::types::Solution;
+
+use std::{collections::HashMap, collections::HashSet, fs::File, io::BufRead, io::BufReader};
+
+pub struct Day4 {
+    pair_map: HashMap<usize, Vec<Vec<u32>>>,
+}
+
+impl<'a> Day4 {
+    fn new(pair_map: HashMap<usize, Vec<Vec<u32>>>) -> Self {
+        Day4 { pair_map }
+    }
+
+    fn part_1(&self) -> u32 {
+        let mut count = 0;
+
+        for (k, v) in &self.pair_map {
+            let (largest_vec, smallest_vec) = Day4::largest_vec(&v[0], &v[1]);
+
+            let set_0: HashSet<_> = largest_vec.iter().collect();
+            let is_subset = smallest_vec.iter().all(|item| set_0.contains(item));
+
+            if is_subset {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    pub fn solution() -> Solution {
+        let file = File::open("src/inputs/day_4_input.txt").expect("Failed to open file");
+        let lines = BufReader::new(file).lines();
+
+        let mut pair_map: HashMap<usize, Vec<Vec<u32>>> = HashMap::new();
+
+        for (index, line) in lines.enumerate() {
+            if let Ok(l) = line {
+                let parsed_sections = Day4::parse_line_into_vec(l);
+                pair_map.insert(index, parsed_sections);
+            }
+        }
+
+        let mut day_4 = Day4::new(pair_map);
+        let part_1_soln = day_4.part_1();
+
+        return Solution {
+            part_1_soln: part_1_soln as u64,
+            part_2_soln: 0,
+        };
+    }
+
+    fn parse_line_into_vec(line: String) -> Vec<Vec<u32>> {
+        let split_line = line
+            .split(",")
+            .map(|group| group.split("-").map(|c| c.parse::<u32>().unwrap()));
+
+        let parsed_sections = split_line
+            .map(|item| {
+                let temp_vec = item.collect::<Vec<u32>>();
+
+                (temp_vec[0]..=temp_vec[1]).collect()
+            })
+            .collect::<Vec<Vec<u32>>>();
+
+        return parsed_sections;
+    }
+
+    fn largest_vec(a: &'a Vec<u32>, b: &'a Vec<u32>) -> (&'a Vec<u32>, &'a Vec<u32>) {
+        if a.len() > b.len() {
+            return (a, b);
+        }
+
+        return (b, a);
+    }
+}
